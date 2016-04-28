6ba4c2b656a7fc149acbfb20270a686297f30e09
Tyler Kim <tkim139@illinois.edu>
2016-04-28 15:29:42 -0500

    Parser

---
+++ Ontologies/parser.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import os
+import codecs
+import subprocess
+from io import StringIO
+
+
+
+def collect(section):
+
+    for num in range(1,101):
+            with open(str(num)+'.json') as data_file:
+                data = json.load(data_file)
+
+                for item in range(0,10):
+                    final_data = (data["response"]["docs"][item][section])
+                    print(final_data)
+
+
+if __name__== "__main__":
+    collect("lead_paragraph")
